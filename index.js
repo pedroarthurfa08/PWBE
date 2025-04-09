c35135778d1c0d2409c60c232241510e5d45a6a1
Pedro Arthur Fontenele Alves <pedroarthurfa08@gmail.com>
2025-04-09 20:55:56 -0300

    Tratamento de requisições com parâmetros

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded({extend: false}))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/ola', (req, res) => {
+    nome = req.body.nome
+    res.send(`Olá ${nome}`)
 })
 
 app.listen(port, () => {
