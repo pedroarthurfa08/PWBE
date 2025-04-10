0d7249bce46222502a43474ece5d42e584970c77
Pedro Arthur Fontenele Alves <pedroarthurfa08@gmail.com>
2025-04-10 19:26:24 -0300

    Corrigindo

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.urlencoded({extend: false}))
+app.use(express.urlencoded({extended: false}))
 
 app.set('viwes', 'views')
 
 app.set('view engine', 'ejs')
 
-app.set("/home_soma", (req, res) => {
+app.get("/home_soma", (req, res) => {
   res.render("home_soma")
 })
 
 app.post("/soma", (req, res) => {
-  n1 = Number(req.body.numero1)
-  n2 = Number(req.body.numero2)
-  soma = n1 + n2
-  res.soma('resposta_soma', {soma: soma})
+  const n1 = Number(req.body.numero1)
+  const n2 = Number(req.body.numero2)
+  const soma = n1 + n2
+  res.render('resposta_soma', {soma: soma})
 })
 
 app.listen(port, () => {
